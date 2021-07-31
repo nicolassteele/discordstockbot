5c9973c0ed2445b9784fc434d35a0bb7b9804df9
nicolassteele <75227009+nicolassteele@users.noreply.github.com>
2021-07-31 16:09:18 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import javax.security.auth.login.LoginException;
 
 public class Main {
     public static JDA jda;
-
+    //login token for discord
     public static void main(String[] args) {
         JDABuilder jdaBuilder = JDABuilder.createDefault("ODE3MjI4NzU4NzEyNTgyMTc1.YEGdlw.oiIx8rYOlh_F6LlDwG-KrirBLII");
 
@@ -17,6 +17,7 @@ public class Main {
         } catch (LoginException e){
             e.printStackTrace();
         }
+        //Shows status on discord and the activity.
         jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
         jda.getPresence().setActivity(Activity.playing("Pogging"));
 
