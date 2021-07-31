db58059bcccf29d016b0ed2409ac595b88d36ef9
nicolassteele <75227009+nicolassteele@users.noreply.github.com>
2021-07-31 14:30:16 -0400

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+import events.CoinPog;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static JDA jda;
+
+    public static void main(String[] args) {
+        JDABuilder jdaBuilder = JDABuilder.createDefault("ODE3MjI4NzU4NzEyNTgyMTc1.YEGdlw.oiIx8rYOlh_F6LlDwG-KrirBLII");
+
+        try {
+            jda = jdaBuilder.build();
+        } catch (LoginException e){
+            e.printStackTrace();
+        }
+        jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+        jda.getPresence().setActivity(Activity.playing("Killing Kevin"));
+
+        jda.addEventListener(new CoinPog());
+    }
+}
---
+++ src/main/java/events/CoinPog.java
@@ -0,0 +1,17 @@
+package events;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class CoinPog extends ListenerAdapter {
+
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        String messageSent = event.getMessage().getContentRaw();
+        if(messageSent.equalsIgnoreCase("hello")){
+            event.getChannel().sendMessage("Hi tuna").queue();
+        }
+        if(messageSent.equalsIgnoreCase("hi")){
+            event.getChannel().sendMessage(".l.").queue();
+        }
+    }
+}
